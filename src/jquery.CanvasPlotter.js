a25319bba9c95e1c6bcacdb962cac1d234454e58
DekusDenial <dekusdenial@hotmail.com>
2012-04-14 16:36:01 -0400

    first structural commit

---
+++ src/jquery.CanvasPlotter.js
@@ -0,0 +1,91 @@
+/*
+ * jquery.CanvasPlotter
+ * https://github.com/DekusDenial/jquery.CanvasPlotter
+ *
+ * Copyright (c) 2012 DekusDenial
+ * Licensed under the MIT license.
+ */
+
+if ( typeof Object.create !== 'function' ) {
+  Object.create = function(o) {
+    function F() {}
+    F.prototype = o;
+    return new F();
+  };
+}
+
+(function($, window, document, undefined){
+
+  var CanvasPlotter = {
+
+    init : function (el, opts){
+      // initialize all data for ground 0
+
+      this.el = el;
+      this.$el = $(el);
+
+      this.config = $.extend( {}, $.fn.canvasPlotter.defaults, opts );
+      this.prepareCanvas();
+    },
+
+    prepareCanvas : function(){
+      // preset the canvas or adjust according to config
+      var $cv = $('<canvas>');
+      if (this.config.animated) {
+        $cv.addClass('bar-animated');
+      }
+
+      this.$el.append($cv);
+
+    },
+
+    fetchData : function() {
+      // read input data, or fetch by AJAX
+    },
+
+    normalizeData : function() {
+      // scale and normalize values to be used in the plotter
+    },
+
+    drawLabels : function() {
+      // draw out XLabels and YLabels
+    },
+
+    plotBars : function(){
+      // plot the bars
+    },
+
+    animatedPlotBars : function(){
+      // plot bars with animation
+    },
+
+    utils : {
+      posX : function(index){
+        // calculate which x position to plot each bar
+      },
+
+      posY : function(index){
+        // calculate the top y position of each bar
+      }
+    }
+  };
+
+  $.fn.canvasPlotter = function(opts) {
+
+    return this.each(function(){
+      var canvasPlotter = Object.create(CanvasPlotter);
+      canvasPlotter.init(this, opts);
+    });
+  };
+
+  $.fn.canvasPlotter.defaults = {
+    // default settings here
+    animated : false,
+    barGap : 15,
+    barWidth : 20,
+    canvasHeight : 200
+
+    // and more ...
+  };
+
+}(jQuery, window, document));
---
+++ test/jquery.CanvasPlotter_test.js
@@ -0,0 +1,57 @@
+/*global QUnit:false, module:false, test:false, asynctest:false, expect:false*/
+/*global start:false, stop:false ok:false, equal:false, notEqual:false, deepEqual:false*/
+/*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
+(function($) {
+
+  /*
+    ======== A Handy Little QUnit Reference ========
+    http://docs.jquery.com/QUnit
+
+    test methods:
+      expect(numAssertions)
+      stop(increment)
+      start(decrement)
+    test assertions:
+      ok(value, [message])
+      equal(actual, expected, [message])
+      notEqual(actual, expected, [message])
+      deepEqual(actual, expected, [message])
+      notDeepEqual(actual, expected, [message])
+      strictEqual(actual, expected, [message])
+      notStrictEqual(actual, expected, [message])
+      raises(block, [expected], [message])
+  */
+
+  module('jQuery#canvasPlotter', {
+    setup : function() {
+      this.elems = $('div#fixture');
+    }
+  });
+
+  test('is available on the jQuery object', 1, function(){
+    ok($.fn.canvasPlotter, 'should be accessible on a collection');
+  });
+
+  test('should be chainable', 1, function(){
+    strictEqual(this.elems.canvasPlotter(), this.elems, 'should return a jQuery object');
+  });
+
+  test('should create a canvas element', 1, function(){
+    this.elems.canvasPlotter();
+    ok(this.elems.find('canvas').length > 0, 'should contain a canvas element');
+  });
+
+  test('should have a default setting object on its namespace', 1, function(){
+    ok($.fn.canvasPlotter.defaults, 'User can change without doing so in the plugin');   
+  });
+
+  test('should allow user to override default settings', 2, function(){
+    this.elems.canvasPlotter({
+      animated : true
+    });
+    var cv = this.elems.find('canvas')[0];
+    ok(cv.className, 'should have class assigned if enabling animation on default setting');
+    strictEqual(cv.className, 'bar-animated', 'should have class "bar-animated" assigned');
+  });
+
+}(jQuery));
